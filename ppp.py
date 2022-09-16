7ac9bb1c96dc3c3cf0180ed8743c3b77e2ec48b2
bhavya080499 <gundlapallebhavyasree2@gmail.com>
2022-09-16 14:38:17 +0530

    3rd

---
+++ ppp.py
@@ -0,0 +1,15 @@
+numbers = {1: 'one', 2: 'two', 3: 'three'}
+dictionaryKeys = numbers.keys()
+print(dictionaryKeys)
+# # # # # #
+list_num = [1,2,3,4]
+tup_num = (1,2,3,4)
+print(list_num)
+print(tup_num)
+# # # # # # #
+a=5
+print('the value of a is' ,a)
+ # # # # # # #
+txt ="bhavya"[::-1]
+print(txt)
+
